cf59ff903a67ab1ec8d3a011036b5376c7cc370a
Guiorgy <Guiorgy123@gmail.com>
2019-01-11 15:40:34 +0400

    fixed a typo in last commit

--- RemoveAndroidPermissions.ps1
+++ RemoveAndroidPermissions.ps1
@@ -1,5 +1,5 @@
 (Get-Content 'obj\Release\*\android\AndroidManifest.xml').replace('<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />', '') | Set-Content 'obj\Release\*\android\AndroidManifest.xml'
-(Get-Content 'obj\Release\*\android\AndroidManifest.xml').replace('<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />', '') | Set-Content 'obj\Release\*\android\AndroidManifest.xml'
+(Get-Content 'obj\Release\*\android\AndroidManifest.xml').replace('<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />', '') | Set-Content 'obj\Release\*\android\AndroidManifest.xml'
 
 <#
     Don't forget to run Command Line (cmd) with administrator rights and execute:
