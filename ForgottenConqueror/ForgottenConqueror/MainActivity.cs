e9e28b5a830e494fc01e3705628b6c7bf26e5299
Guiorgy <Guiorgy123@gmail.com>
2019-01-17 11:42:21 +0400

    Do not let the launcher create a new activity
    http://stackoverflow.com/questions/16283079

--- ForgottenConqueror/ForgottenConqueror/MainActivity.cs
+++ ForgottenConqueror/ForgottenConqueror/MainActivity.cs
@@ -24,6 +24,17 @@ namespace ForgottenConqueror
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            /// Do not let the launcher create a new activity http://stackoverflow.com/questions/16283079
+            if (!IsTaskRoot)
+            {
+                /// Android launched another instance of the root activity into an existing task
+                ///  so just quietly finish and go away, dropping the user back into the activity
+                ///  at the top of the stack (ie: the last state of this task)
+                Finish();
+                return;
+            }
+
             SetContentView(Resource.Layout.widget_configuration);
 
 #if !RELEASE
